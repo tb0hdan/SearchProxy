bdd1eeb969f0a406fbfdc81e10db4309852da735
Bohdan Turkynewych <tb0hdan@gmail.com>
2019-12-03 01:35:36 +0200

    flags

--- main.go
+++ main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"flag"
+	"path"
+	"strings"
 
 	"searchproxy/server"
 )
 
 func main() {
 	var (
-		debug       = flag.Bool("debug", false, "enable debug")
-		geoIPDBFile = flag.String("geoipdb", "GeoLite2-City.mmdb", "Path to Maxmind's DB file")
+		debug        = flag.Bool("debug", false, "enable debug")
+		geoIPDBFile  = flag.String("geoipdb", "GeoLite2-City.mmdb", "Path to Maxmind's DB file")
+		bind         = flag.String("bind", "0.0.0.0:8000", "Address to bind to, host:port")
+		readTimeout  = flag.Int("readt", 30, "Read timeout, seconds")
+		writeTimeout = flag.Int("writet", 30, "Write timeout, seconds")
+		mirrorsPath  = flag.String("mirrors", "./mirrors.yml", "Path to mirrors.yml file")
 	)
 
 	flag.Parse()
 
-	searchProxyServer := server.New("0.0.0.0:8000", 30, 30)
+	searchProxyServer := server.New(*bind, *readTimeout, *writeTimeout)
 	searchProxyServer.SetDebug(*debug)
 	searchProxyServer.SetGeoIPDBFile(*geoIPDBFile)
-	searchProxyServer.ConfigFromFile("mirrors", ".")
+
+	directory, file := path.Split(*mirrorsPath)
+	filePattern := strings.TrimRight(file, ".yml")
+
+	searchProxyServer.ConfigFromFile(filePattern, directory)
 	searchProxyServer.Run()
 }
